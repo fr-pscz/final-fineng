e3cea54c539dfe7bff074e23085648e02511e757
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-05-31 10:16:53 +0200

    Update bootstrapEONIA.m

--- Code/calibration/bootstrapEONIA.m
+++ Code/calibration/bootstrapEONIA.m
@@ -1,6 +1,5 @@
 function P = bootstrapEONIA(OIS)
-
-% Does bootstrap of EONIA curve
+%BOOTSTRAPEONIA Bootstrap discounts from quoted OIS rates
 %
 % INPUTS:
 % OIS: struct with market information contained in 4 fields: 
@@ -8,57 +7,57 @@ function P = bootstrapEONIA(OIS)
 %             - settledate
 %             - maturity
 %             - rate
-% (all dates in datenum format, rate in %)
+% (all dates in datenum format)
 %
 % OUTPUTS: 
-% P: struct with dates and discounts of OIS rates 
+% P: struct with dates and EONIA discounts
 
 
 %% Extracting arrays from inputs
 
 settleDate = OIS.settledate;
 maturity = [OIS.maturity]';
-rate = [OIS.rate]' /100;
+rate = [OIS.rate]';
 
 
-%% Bootstrap of OIS with maturity less than 1 year
+%% Computing discounts with maturity up to 1 year
 
-% selecting dates and rates with maturity less than 1 year
-maturityBefore1Y = maturity(1:8);
-ratesBefore1Y = rate(1:8);
+% selecting dates and rates with maturity up to 1 year
+oneYear = dateRolling(datenum(datetime(settleDate, "ConvertFrom", "datenum") + calyears(1)), 'follow');
+maturityBefore1Y = maturity(maturity <= oneYear);
+ratesBefore1Y = rate(maturity <= oneYear);
 
 % computing yearfraction for bootstrap
-act360 = 2;
-deltaT0Te = yearfrac(settleDate, maturityBefore1Y, act360); 
+convOIS = 2; % Act/360
+deltaT0Te = yearfrac(settleDate, maturityBefore1Y, convOIS); 
 
 % computing discounts
 discountsBefore1Y = 1 ./ (1 + deltaT0Te .* ratesBefore1Y);
 
 
-%% Boostrap of OIS with maturity longer than 1 year 
+%% Boostrap discounts with maturity longer than 1 year 
 
 % selecting dates and rates with maturity longer than 1 year
-maturityAfter1Y = maturity(9:end);
-rateAfter1Y = rate(9:end);
+maturityAfter1Y = maturity(maturity > oneYear);
+rateAfter1Y = rate(maturity > oneYear);
 
 % computing array of yearfractions: delta(t_i, t_i+1) for i=1,2,...,11
-deltaI = yearfrac([maturityBefore1Y(end); maturityAfter1Y(1:end-1)], maturityAfter1Y, act360);
+deltaI = yearfrac([maturityBefore1Y(end); maturityAfter1Y(1:end-1)], maturityAfter1Y, convOIS);
 
-% inizializing axuliary variables
-bpv = yearfrac (settleDate, maturityBefore1Y(end), act360) * discountsBefore1Y(end);
+% inizializing auxiliary variables
+bpv = yearfrac(settleDate, maturityBefore1Y(end), convOIS) * discountsBefore1Y(end); % 1Y BPV
 discountAfter1Y = zeros(length(deltaI),1);
 
-% computing discounts
+% bootstrap
 for i = 1:length(deltaI)
-    
+    % compute discount
     discountAfter1Y(i) = ( 1 - rateAfter1Y(i) * bpv ) / ( 1 + deltaI(i) * rateAfter1Y(i) );
-    
+    % update BPV for next iteration
     bpv = bpv + deltaI(i) * discountAfter1Y(i);
 end
 
 % Assigning output values
-P.y = [discountsBefore1Y; discountAfter1Y];
-
-P.t = maturity;
+P.y = [1; discountsBefore1Y; discountAfter1Y];
+P.t = [settleDate; maturity];
 
 end
