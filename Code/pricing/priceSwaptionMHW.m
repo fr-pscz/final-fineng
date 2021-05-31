923dfca9b9d6b14f01f5da8dc0312c238b387ba1
fravalent <84444834+fravalent@users.noreply.github.com>
2021-05-31 17:13:39 +0200

    Update priceSwaptionMHW.m

--- Code/pricing/priceSwaptionMHW.m
+++ Code/pricing/priceSwaptionMHW.m
@@ -1,4 +1,4 @@
-function PRICE = priceSwaptionMHW(MKTSWAPTION, DISCOUNT, PSEUDODISCOUNT)
+function PRICE = priceSwaptionMHW(MKTSWAPTION, PD, P)
 %
 %
 %
@@ -6,24 +6,31 @@ function PRICE = priceSwaptionMHW(MKTSWAPTION, DISCOUNT, PSEUDODISCOUNT)
 
 act365 = 3;
 
-f = fun(MKTSWAPTION, DISCOUNT, PSEUDODISCOUNT);
+f = fun(MKTSWAPTION, PD, P);
 
 xstar = @(p) fzero(@(x) f(x,p),0)
+
+
 
 tenor = yearfrac(MKTSWAPTION.optionmaturity,MKTSWAPTION.swapmaturity,act365);
 c = @(m,s) 1./s*(1 - 1/(1+s./m)^(tenor));
 
-s = computeS(MKTSWAPTION, DISCOUNT, PSEUDODISCOUNT);
+s = computeS(MKTSWAPTION, PD, P);
 
-strikeATM = swapRate(MKTSWAPTION, DISCOUNT, PSEUDODISCOUNT);
+strikeATM = swapRate(MKTSWAPTION, PD, P);
 
-discountOptionMaturity = findDiscount(MKTSWAPTION.optionmaturity,DISCOUNT);
+discountOptionMaturity = findDiscount(MKTSWAPTION.optionmaturity,PD);
 m = 1;
 integrand = @(x,p) exp(-x.^2/2)/sqrt(2*pi)*c(m,s(x,p))*max((strikeATM - s(x,p)),0).*(x < xstar(p));
 
-% PRICE = @(p) discountOptionMaturity * quadgk(@(x) integrand(x,p),-inf,+inf);
+% PRICEq = @(p) discountOptionMaturity * quadgk(@(x) integrand(x,p),-inf,+inf);
 PRICE = @(p) discountOptionMaturity * integral(@(x) integrand(x,p),-inf,+inf,'arrayvalued',true);
+
+
 end
+
+
+
 
 
 
