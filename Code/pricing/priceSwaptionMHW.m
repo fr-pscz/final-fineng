da7bbb4dde90f79f36f5a97707a5546ff37c6520
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-04 18:55:21 +0200

    Update priceSwaptionMHW.m

--- Code/pricing/priceSwaptionMHW.m
+++ Code/pricing/priceSwaptionMHW.m
@@ -1,36 +1,9 @@
-function PRICE = priceSwaptionMHW(MKTSWAPTION, PD, P)
-%
-%
-%
-%
+function PX = priceSwaptionMHW(MKTSWAPTION, PD, P, PARAM)
 
-act365 = 3;
+PX = zeros(numel(MKTSWAPTION),1);
 
-f = fun(MKTSWAPTION, PD, P);
-
-xstar = @(p) fzero(@(x) f(x,p),0)
-
-
-
-tenor = yearfrac(MKTSWAPTION.optionmaturity,MKTSWAPTION.swapmaturity,act365);
-c = @(m,s) 1./s*(1 - 1/(1+s./m)^(tenor));
-
-s = computeS(MKTSWAPTION, PD, P);
-
-strikeATM = swapRate(MKTSWAPTION, PD, P);
-
-discountOptionMaturity = findDiscount(MKTSWAPTION.optionmaturity,PD);
-m = 1;
-integrand = @(x,p) exp(-x.^2/2)/sqrt(2*pi)*c(m,s(x,p))*max((strikeATM - s(x,p)),0).*(x < xstar(p));
-
-% PRICEq = @(p) discountOptionMaturity * quadgk(@(x) integrand(x,p),-inf,+inf);
-PRICE = @(p) discountOptionMaturity * integral(@(x) integrand(x,p),-inf,+inf,'arrayvalued',true);
-
-
+for ii = 1:numel(MKTSWAPTION)
+    f = priceFunctionCSSMHW(MKTSWAPTION(ii), PD, P);
+    PX(ii) = f(PARAM);
 end
-
-
-
-
-
-
+end % priceSwaptionMHW
