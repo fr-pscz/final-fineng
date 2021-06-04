59c9985e5b248acf4865fa287064eaaa3848fcea
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-04 22:40:22 +0200

    Update swapRate.m

--- Code/utilities/swapRate.m
+++ Code/utilities/swapRate.m
@@ -1,57 +1,67 @@
-function RATE = swapRate(MKTSWAPTION, DISCOUNT, PSEUDODISCOUNT)
+function RATE = swapRate(MKTSWAPTION, PD, P)
+%SWAPRATE Compute par swap rate for the swap underlying a given swaption
 %
+% INPUTS:
+% MKTSWAPTION: struct with following fields: 
+%             - optionmaturity
+%             - swapmaturity
 %
+%          PD: struct with dates and OIS-adjusted discounts
+%           P: struct with dates and EUR6M pseudodiscounts 
 %
+% OUTPUTS: 
+%        RATE: scalar rate
 %
-%
+% FUNCTIONS:
+%  paymentDates, findDiscount
 
+%% Daycount conventions for fixed leg
 
-act365 = 3;
-act360 = 2;
+fixedConv = 6; % 30/360
 
 %% Payment dates
 
 % generate an array containing the dates of the floating payments and the
 % corresponding discounts
-freqFloating = 2;
-floatingDates = paymentDates(MKTSWAPTION.optionmaturity, MKTSWAPTION.swapmaturity, freqFloating, 'follow');
-floatingDiscounts = findDiscount(floatingDates,DISCOUNT);
+floatingFreq = 2;
+floatingDates = paymentDates(MKTSWAPTION.optionmaturity, MKTSWAPTION.swapmaturity, floatingFreq, 'follow');
+floatingDiscounts = findDiscount(floatingDates,PD);
 
 % generate an array containing the dates of the fixed payments and the
 % corresponding discounts
-freqFixed = 1;
-fixedDates = paymentDates(MKTSWAPTION.optionmaturity, MKTSWAPTION.swapmaturity, freqFixed, 'follow');
-fixedDiscounts = findDiscount(fixedDates,DISCOUNT);
+fixedFreq = 1;
+fixedDates = paymentDates(MKTSWAPTION.optionmaturity, MKTSWAPTION.swapmaturity, fixedFreq, 'follow');
+fixedDiscounts = findDiscount(fixedDates,PD);
 
 
 %% Fixed and floating forward discounts
 
 % array of delta between all fixed payment dates. The first element is wrt
 % the option maturity
-delta = yearfrac(fixedDates(1:end-1),fixedDates(2:end),act360);
+delta = yearfrac(fixedDates(1:end-1),fixedDates(2:end),fixedConv);
 
 % forward fixed discount wrt option maturity
-fwdFixedDiscountsAlfa = fixedDiscounts(2:end)/fixedDiscounts(1);  % fwd rispetto a alfa
+fwdFixedDiscounts = fixedDiscounts(2:end)/fixedDiscounts(1);
 
 % floating forward discounts wrt option maturity
-fwdFloatingDiscountsAlfa = floatingDiscounts(1:end-1)/floatingDiscounts(1); % B alfa primo i (fwd rispetto ad alfa, mi fermo a omega-1)
+fwdFloatingDiscounts = floatingDiscounts(1:end-1)/floatingDiscounts(1); 
 
 %% Spread
 % floating forward discounts
-fwdFloatingDiscounts = floatingDiscounts(2:end)./floatingDiscounts(1:end-1);
+fwdDiscounts = floatingDiscounts(2:end)./floatingDiscounts(1:end-1);
 
 % pseudodiscounts and forward pseudodiscounts at floating dates
-floatingPseudoDiscounts = findDiscount(floatingDates,PSEUDODISCOUNT);
-fwdFloatingPseudoDiscounts = floatingPseudoDiscounts(2:end)./floatingPseudoDiscounts(1:end-1);
+floatingPseudoDiscounts = findDiscount(floatingDates,P);
+fwdPseudodiscounts = floatingPseudoDiscounts(2:end)./floatingPseudoDiscounts(1:end-1);
 
-spread = fwdFloatingDiscounts./fwdFloatingPseudoDiscounts;
+spread = fwdDiscounts./fwdPseudodiscounts;
 
 %% Swap rate
 
 % bpv relative to fixed dates
-bpv = sum(delta.*fwdFixedDiscountsAlfa);
+bpv = sum(delta.*fwdFixedDiscounts);
 
 % swap rate
-RATE = (1 - fwdFixedDiscountsAlfa(end) + sum(fwdFloatingDiscountsAlfa.*(spread - 1)))/bpv;
+RATE = (1 - fwdFixedDiscounts(end) + sum(fwdFloatingDiscounts.*(spread - 1)))/bpv;
 
 end % swapRate
