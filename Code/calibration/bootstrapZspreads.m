31cec0526fe8ba9ca24ed27c1a3fb3bd695f1e1a
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-07 13:43:50 +0200

    Update bootstrapZspreads.m

--- Code/calibration/bootstrapZspreads.m
+++ Code/calibration/bootstrapZspreads.m
@@ -1,7 +1,9 @@
-function [outputArg1,outputArg2] = getSwapRate(inputArg1,inputArg2)
-%GETSWAPRATE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function Z = bootstrapZspreads(MKTBOND,PD)
+
+Z = getZeta(MKTBOND(1), PD);
+
+for ii = 2:numel(MKTBOND)
+    Z = getZeta(MKTBOND(ii), PD, Z);
 end
 
+end
