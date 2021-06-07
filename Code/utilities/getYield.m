6b3e47f3e8034d97ca3bd3f531e67175b8ac4d9a
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-07 13:48:19 +0200

    Update getYield.m

--- Code/utilities/getYield.m
+++ Code/utilities/getYield.m
@@ -1,7 +1,17 @@
-function [outputArg1,outputArg2] = getSwapRate(inputArg1,inputArg2)
-%GETSWAPRATE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function Y = getYield(MKTBOND)
 
+convYield = 3; % Act/365
+% compute payment periods and coupons+notional
+deltas   = yearfrac(MKTBOND.paymentdates(1:end-1),MKTBOND.paymentdates(2:end),MKTBOND.daycount);
+payments = MKTBOND.coupon.*100.*ones(numel(deltas),1).*deltas;
+payments(end) = payments(end) + 100;
+% function handle for dirty price
+f = @(y) sum(...
+    payments.*exp(-y.*yearfrac(MKTBOND.settledate, MKTBOND.paymentdates(2:end),convYield))...
+    );
+% find yield
+Y = fzero(@(y)...
+    f(y) - MKTBOND.px - MKTBOND.coupon.*yearfrac(...
+    MKTBOND.paymentdates(1),MKTBOND.settledate,MKTBOND.daycount)...
+    , 0);
+end % getYield
