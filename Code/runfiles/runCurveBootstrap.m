4091a44ab228d13aadb6c02ad5e37a3b4a2b415d
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-09 22:48:11 +0200

    Update runCurveBootstrap.m

--- Code/runfiles/runCurveBootstrap.m
+++ Code/runfiles/runCurveBootstrap.m
@@ -4,13 +4,23 @@ addpath('utilities')
 addpath('calibration')
 addpath('pricing')
 addpath('data')
+addpath('plots')
 
 
 %% Load data
 load('market.mat')
 
-PD = bootstrapEONIA(mktOIS);
+PD      = bootstrapEONIA(mktOIS);
+PD.name = 'EONIA';
 zD = disc2zero(PD.y(2:end), PD.t(2:end), PD.t(1), -1, 3);
 
 P  = bootstrapEUR6M(mktFRA, mktSwap, PD);
+P.name = 'Euribor 6M';
 z  = disc2zero(P.y(2:end), P.t(2:end), P.t(1), -1, 3);
+
+%% Plot
+plotCurve([PD; P])
+
+%% Save output
+scriptOutputs = {'PD', 'P', 'zD', 'z'};
+save('data/curves.mat', scriptOutputs{:})
