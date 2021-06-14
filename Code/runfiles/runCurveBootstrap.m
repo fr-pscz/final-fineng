e740351c73dc127a0871022374fa9ab46dc8612d
EttoreBusani <81577593+EttoreBusani@users.noreply.github.com>
2021-06-14 15:24:05 +0200

    Update runCurveBootstrap.m
    adding plot with different interpolation rules

--- Code/runfiles/runCurveBootstrap.m
+++ Code/runfiles/runCurveBootstrap.m
@@ -18,9 +18,24 @@ P  = bootstrapEUR6M(mktFRA, mktSwap, PD);
 P.name = 'Euribor 6M';
 z  = disc2zero(P.y(2:end), P.t(2:end), P.t(1), -1, 3);
 
+figure(1)
 %% Plot
 plotCurve([PD; P])
+
 
 %% Save output
 scriptOutputs = {'PD', 'P', 'zD', 'z'};
 save('data/curves.mat', scriptOutputs{:})
+
+
+figure(2)
+P  = bootstrapAlternativeEUR6M(mktFRA, mktSwap, PD);
+plot(P.t, P.y, '-', 'LineWidth', 2);
+
+for ii = 1:3
+    hold on
+    P1  = bootstrapAlternativeEUR6M(mktFRA, mktSwap, PD, ii); 
+    plot(P1.t, P1.y, '-', 'LineWidth', 2);
+    fprintf('Max interpolation error using FLAG = %d is %e \n ', [ii; max(abs(P.y - P1.y))] )
+end
+legend('Linear on rates', 'Spline on rates', 'Spline on disc', 'log linear on disc');
