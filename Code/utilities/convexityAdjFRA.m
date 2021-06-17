7d165f79557d80d172798de89247b9ad5738d345
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-17 12:08:26 +0200

    Update convexityAdjFRA.m

--- Code/utilities/convexityAdjFRA.m
+++ Code/utilities/convexityAdjFRA.m
@@ -1,1 +1,31 @@
+function MKTFRACONVEX = convexityAdjFRA(MKTFRA, PARAMS)
+%CONVEXITYADJFRA Convexity adjustment for forward rate agreements
+%
+% INPUTS:
+%   MKTFRA: struct with following fields: 
+%
+%   PARAMS:          struct with dates and OIS-adjusted discounts
+%
+% OUTPUTS: 
+%
+% FUNCTIONS:
 
+convVol = 3; % Act/365
+convFRA = 2; % Act/360
+% initialize output struct
+MKTFRACONVEX = MKTFRA;
+
+for ii = 1:numel(MKTFRA)
+    sigma = @(t) PARAMS(2).*exp(PARAMS(1).*t).*...
+        (exp(-PARAMS(1).*yearfrac(MKTFRA(ii).settledate, MKTFRA(ii).startdate, convVol))...
+       - exp(-PARAMS(1).*yearfrac(MKTFRA(ii).settledate, MKTFRA(ii).enddate, convVol)))./PARAMS(1);
+    eta   = @(t) (1-PARAMS(3)).*PARAMS(2).*exp(PARAMS(1).*t).*...
+        (exp(-PARAMS(1).*yearfrac(MKTFRA(ii).settledate, MKTFRA(ii).startdate, convVol))...
+       - exp(-PARAMS(1).*yearfrac(MKTFRA(ii).settledate, MKTFRA(ii).enddate, convVol)))./PARAMS(1);
+   
+   gammaFRA = exp(-integral(@(t) sigma(t) .* eta(t), 0, yearfrac(MKTFRA(ii).settledate, MKTFRA(ii).startdate, convVol)));
+   
+   MKTFRACONVEX(ii).rate = MKTFRA(ii).rate.*gammaFRA + (gammaFRA - 1)./yearfrac(MKTFRA(ii).startdate, MKTFRA(ii).enddate, convFRA);
+end % cycle through FRAs
+
+end 
