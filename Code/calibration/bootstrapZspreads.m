9aec397820b0ed7b6e4813b5587ad78c3ab7896a
fravalent <84444834+fravalent@users.noreply.github.com>
2021-06-17 14:50:44 +0200

    Update bootstrapZspreads.m

--- Code/calibration/bootstrapZspreads.m
+++ Code/calibration/bootstrapZspreads.m
@@ -1,9 +1,31 @@
 function Z = bootstrapZspreads(MKTBOND,PD)
+% BOOTSTRAPZSPREADS function that bootstraps the Z spreads
+%
+% INPUTS:
+% MKTBOND: struct with the following fields:
+%             - maturity
+%             - valuedate
+%             - settledate
+%             - px
+%             - coupon
+%             - yield
+%             - payment dates
+%             - daycount
+%
+% PD: struct with dates and OIS-adjusted discounts 
+%
+% OUTPUTS: 
+% Z: struct with the following fields:
+%             - t: dates
+%             - y: Z-spreads
+%
+% FUNCTIONS:
+% getZeta
 
 Z = getZeta(MKTBOND(1), PD);
 
 for ii = 2:numel(MKTBOND)
     Z = getZeta(MKTBOND(ii), PD, Z);
-end
+end % cycle on all the available bonds from the market
 
-end
+end %bootstrapZspreads
