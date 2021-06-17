3af2c6e5c44499995b31ff6c91be9b40ea8f86e0
Francesco Pascuzzi <francesco.pas7@gmail.com>
2021-06-17 10:58:45 +0200

    Add files via upload

---
+++ Code/runfiles/runSwaptionCalibrationConvexity.m
@@ -0,0 +1,32 @@
+clear
+clc
+addpath('utilities')
+addpath('calibration')
+addpath('pricing')
+addpath('data')
+addpath('plots')
+
+%% Load data
+load('market.mat')
+load('curvesConvexity.mat')
+
+%% Get market prices
+for ii = 1:numel(mktSwaption)
+    mktSwaption(ii).strike = swapRate(mktSwaption(ii), PD, P);
+    mktSwaption(ii).px     = priceSwaption(mktSwaption(ii), PD, P);
+end
+
+%% Minimize error
+params = calibrateMHW(mktSwaption, PD, P);
+
+%% Get model prices
+priceMHW = priceSwaptionMHW(mktSwaption,PD, P, params);
+
+%% Plot
+plotSwaptions([mktSwaption.px], priceMHW)
+disp('Parameters:')
+disp(['   > a = ' num2str(params(1))])
+disp(['   > σ = ' num2str(params(2))])
+disp(['   > γ = ' num2str(params(3))])
+%% Save output
+save('data/paramsConvexity.mat', 'params')
