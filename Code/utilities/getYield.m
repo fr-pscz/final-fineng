aebd787d1369a851a0dba8135168b8c574be51d5
EttoreBusani <81577593+EttoreBusani@users.noreply.github.com>
2021-06-17 14:54:47 +0200

    Update getYield.m
    added comments

--- Code/utilities/getYield.m
+++ Code/utilities/getYield.m
@@ -1,5 +1,20 @@
 function Y = getYield(MKTBOND)
 
+% GETYIELD computes the yield of a given Bond
+%
+% INPUTS: 
+% MKTBOND: struct containing the infromation of the bond in the following fields
+%                       - settledate: settlement date 
+%                       - paymentdates: array of payment dates of the coupons of the bond, considering next-buisness date convenction
+%                       - coupon: array of coupons in percentage
+%                       - daycount: daycount convenction used for the coupons
+%                       - invoice: dirty price of the bond (if not present the function computes the clean price)
+%
+%
+% OUTPUTS
+% Y: Yield of the given bond 
+
+
 convYield = 3; % Act/365
 % compute payment periods and coupons+notional
 deltas   = yearfrac(MKTBOND.paymentdates(1:end-1),MKTBOND.paymentdates(2:end),MKTBOND.daycount);
