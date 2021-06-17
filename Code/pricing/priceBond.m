38bbe32031113e0e55640fb27744bfe5be1274ef
EttoreBusani <81577593+EttoreBusani@users.noreply.github.com>
2021-06-17 15:23:53 +0200

    Update priceBond.m
    added comments

--- Code/pricing/priceBond.m
+++ Code/pricing/priceBond.m
@@ -1,5 +1,20 @@
 function PX = priceBond(MKTBOND, DISCCURVE)
 
+% PRICEBOND computes the clean price of the given Bond
+%
+% INPUTS: 
+% MKTBOND: struct containing the infromation of the bond in the following fields
+%                       - settledate:   settlement date 
+%                       - paymentdates: array of payment dates of the coupons of the bond, considering next-buisness date convenction
+%                       - coupon:       array of coupons in percentage
+%                       - daycount:     daycount convenction used for the coupons
+%
+% OUTPUTS: 
+% PX:      clean price of the given bond.
+%
+% FUNCTIONS:
+% findDiscount
+
 deltas   = yearfrac(MKTBOND.paymentdates(1:end-1),MKTBOND.paymentdates(2:end),MKTBOND.daycount);
 payments = MKTBOND.coupon.*100.*ones(numel(deltas),1).*deltas;
 payments(end) = payments(end) + 100;
