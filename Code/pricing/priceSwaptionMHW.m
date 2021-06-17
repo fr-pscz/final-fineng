7600b4fd7513126e81f0fcefc2235a5f4e1a1ae2
fravalent <84444834+fravalent@users.noreply.github.com>
2021-06-17 15:13:28 +0200

    Update priceSwaptionMHW.m

--- Code/pricing/priceSwaptionMHW.m
+++ Code/pricing/priceSwaptionMHW.m
@@ -1,4 +1,23 @@
 function PX = priceSwaptionMHW(MKTSWAPTION, PD, P, PARAM)
+% PRICESWAPTIONMHW function that computes the price of swaptions according to a multi-curve Hull and White
+% INPUTS:
+% MKTSWAPTION: struct with following fields:
+%             - settledate
+%             - optionmaturity
+%             - swapmaturity
+%             - strike
+%             - px
+%
+% PD: struct with dates and OIS-adjusted discounts
+% P: struct with dates and EUR6M pseudodiscounts 
+%
+% PARAM: array containing the parameters of the model
+%
+% OUTPUTS:
+% PX: price of the swaptions
+%
+% FUNCTIONS:
+% priceFunctionCSSMHW
 
 PX = zeros(numel(MKTSWAPTION),1);
 
